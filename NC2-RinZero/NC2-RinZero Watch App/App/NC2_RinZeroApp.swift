62b9bf3adf51841ec816ec39d7ce81cb7741be7c
Seo Jooyoung <jooyoungseo02@gmail.com>
2024-06-18 17:06:18 +0900

    Create Project

---
+++ NC2-RinZero/NC2-RinZero Watch App/App/NC2_RinZeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  NC2_RinZeroApp.swift
+//  NC2-RinZero Watch App
+//
+//  Created by Seo-Jooyoung on 6/18/24.
+//
+
+import SwiftUI
+
+@main
+struct NC2_RinZero_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NC2-RinZero/NC2-RinZero Watch App/Constant/Color.swift
@@ -0,0 +1,15 @@
+//
+//  Color.swift
+//  NC2-RinZero Watch App
+//
+//  Created by Seo-Jooyoung on 6/18/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct AppColor {
+    static let main = Color(.pink)
+    static let black = Color(.black)
+    static let white = Color(.white)
+}
