7828fa55eaa565e829218462d64aa821a7b1f7ac
Taerin Kim <[taerin1109@gmail.com]>
2024-06-19 23:13:19 +0900

    feat: AvgPaceView 구현

--- NC2-RinZero/NC2-RinZero Watch App/View/AvgPaceView.swift
+++ NC2-RinZero/NC2-RinZero Watch App/View/AvgPaceView.swift
@@ -9,9 +9,45 @@ import SwiftUI
 
 struct AvgPaceView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            ZStack {
+                Image("Frame 18")
+                    .resizable()
+                    .frame(width: 250, height: 250)
+                VStack{
+                    Text("6'00''")
+                        .foregroundStyle(.white)
+                        .font(.system(size: 36, weight: .bold))
+                    Text("Avg. Pace")
+                        .font(.system(size: 12, weight: .regular))
+                        .opacity(0.4)
+                }
+            }
+            .padding(.bottom, -10)
+            HStack {
+                VStack {
+                    Text("5")
+                        .font(.system(size: 20, weight: .medium))
+                    Text("km")
+                        .font(.system(size: 12, weight: .regular))
+                        .opacity(0.4)
+                }
+                Spacer()
+                VStack {
+                    Text("30")
+                        .font(.system(size: 20, weight: .medium))
+                    Text("min")
+                        .font(.system(size: 12, weight: .regular))
+                        .opacity(0.4)
+                }
+            }
+            .padding(.horizontal)
+            .padding(.top, -70)
+        }
     }
 }
+
+
 
 #Preview {
     AvgPaceView()
