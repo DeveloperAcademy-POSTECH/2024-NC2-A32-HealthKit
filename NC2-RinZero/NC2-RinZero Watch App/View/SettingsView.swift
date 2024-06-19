2df7911692090209531defd8d16e6e559be322d4
Taerin Kim <[taerin1109@gmail.com]>
2024-06-19 23:11:39 +0900

    feat: SettingView 구현

--- NC2-RinZero/NC2-RinZero Watch App/View/SettingsView.swift
+++ NC2-RinZero/NC2-RinZero Watch App/View/SettingsView.swift
@@ -9,7 +9,52 @@ import SwiftUI
 
 struct SettingsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        
+        
+        NavigationStack {
+            List {
+                NavigationLink(destination: DistanceInputView()) {
+                    HStack {
+                        VStack {
+                            Text("Distance")
+                                .font(.system(size: 12))
+                                .opacity(0.5)
+                                .padding(.trailing, 10)
+                            Text("5km")
+                                .font(.system(size: 35))
+                        }
+                        .padding()
+                        Image(systemName: "chevron.forward")
+                            .resizable()
+                            .frame(width: 10, height: 20)
+                            .opacity(0.5)
+                            .padding(.leading, 60)
+                    }
+                }
+                NavigationLink(destination: TimeInputView()) {
+                    HStack {
+                        VStack {
+                            Text("Time")
+                                .font(.system(size: 12))
+                                .opacity(0.5)
+                                .padding(.trailing, 60)
+                            Text("30min")
+                                .font(.system(size: 35))
+                        }
+                        .padding()
+                        Image(systemName: "chevron.forward")
+                            .resizable()
+                            .frame(width: 10, height: 20)
+                            .opacity(0.5)
+                            .padding(.leading, 30)
+                    }
+                }
+            }
+            .listStyle(CarouselListStyle())
+            .padding(.top, 10)
+        }
+        .navigationTitle("Settings")
     }
 }
 
