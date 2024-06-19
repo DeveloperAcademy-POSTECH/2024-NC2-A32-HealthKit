522a83a618e4830bdaadbbd6b8be1d978d592313
Taerin Kim <[taerin1109@gmail.com]>
2024-06-19 23:15:35 +0900

    feat: EndResumeView 구현

--- NC2-RinZero/NC2-RinZero Watch App/View/EndResumeView.swift
+++ NC2-RinZero/NC2-RinZero Watch App/View/EndResumeView.swift
@@ -4,12 +4,44 @@
 //
 //  Created by Taerin Kim on 6/19/24.
 //
-
 import SwiftUI
 
 struct EndResumeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            VStack {
+                ZStack {
+                    Rectangle()
+                        .fill(.white)
+                        .opacity(0.5)
+                        .frame(width: 78, height: 50)
+                        .cornerRadius(24)
+                    Rectangle()
+                        .fill(.black)
+                        .opacity(0.6)
+                        .frame(width: 20, height: 20)
+                }
+                Text("End")
+                    .font(.system(size: 12))
+                    .opacity(0.8)
+            }
+            .padding(.trailing, 8)
+            VStack {
+                ZStack {
+                    Rectangle()
+                        .fill(.pink)
+                        .frame(width: 78, height: 50)
+                        .cornerRadius(24)
+                    Image(systemName: "arrow.clockwise")
+                        .resizable()
+                        .frame(width: 20, height: 25)
+                        .fontWeight(.bold)
+                }
+                Text("Resume")
+                    .font(.system(size: 12))
+                    .opacity(0.8)
+            }
+        }
     }
 }
 
