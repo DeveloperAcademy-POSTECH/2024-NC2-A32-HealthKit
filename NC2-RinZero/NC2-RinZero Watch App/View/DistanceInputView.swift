a4d532061c0930ad0816c3ad39383734909d1853
Taerin Kim <[taerin1109@gmail.com]>
2024-06-19 23:25:26 +0900

    feat: Setting DetailView 구현

--- NC2-RinZero/NC2-RinZero Watch App/View/DistanceInputView.swift
+++ NC2-RinZero/NC2-RinZero Watch App/View/DistanceInputView.swift
@@ -8,11 +8,72 @@
 import SwiftUI
 
 struct DistanceInputView: View {
+    @State private var selectedNumber = 1
+    
+    let numbers = Array(1...100)
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+//        NavigationView {
+        VStack {
+            VStack {
+                VStack {
+                    Picker("Distance", selection: $selectedNumber) {
+                        ForEach(numbers, id: \.self) { number in
+                            Text("\(number)").tag(number)
+                                .foregroundStyle(.white)
+                        }
+                    }
+                    .foregroundStyle(Color.pink)
+                    .pickerStyle(.wheel)
+                    .overlay(
+                        VStack {
+                            Spacer().frame(height: 18)
+                            Rectangle()
+                                .fill(.clear)
+                                .stroke(Color.pink, lineWidth: 5)
+                                .cornerRadius(10)
+                        }
+                    )
+                    // Text("Selected number: \(selectedNumber)")
+                    // .padding()
+                }
+                // .navigationTitle("Distance")
+                .frame(width: 180, height: 100)
+                .clipped()
+                // .padding(.bottom, 20)
+            }
+            HStack {
+                VStack {
+                    Button(action: {
+                        print()
+                    }) {
+                        Image(systemName: "multiply")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                    }
+                    .padding(.trailing, 40)
+                }
+                VStack {
+                    Button(action: {
+                        print()
+                    }) {
+                        Image(systemName: "checkmark")
+                            .font(.headline)
+                            .foregroundColor(.pink)
+                    }
+                }
+                .padding(.leading, 40)
+            }
+            .padding(.top, 10)
+        }
+        .padding(.top, 20)
     }
 }
 
-#Preview {
-    DistanceInputView()
+
+struct DistanceInputView_Previews: PreviewProvider {
+    static var previews: some View {
+        DistanceInputView()
+    }
 }
+
--- NC2-RinZero/NC2-RinZero Watch App/View/TimeInputView.swift
+++ NC2-RinZero/NC2-RinZero Watch App/View/TimeInputView.swift
@@ -8,11 +8,87 @@
 import SwiftUI
 
 struct TimeInputView: View {
+    @State private var selectedHour = 0
+    @State private var selectedMinute = 0
+    
+    let hours = Array(0...23)
+    let minutes = Array(0...59) 
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                HStack {
+                    CustomPicker(selectedValue: $selectedHour, range: hours, label: "Hour")
+                    CustomPicker(selectedValue: $selectedMinute, range: minutes, label: "Minute")
+                }
+                .frame(height: 120)
+//                Text("Selected time: \(String(format: "%02d", selectedHour)):\(String(format: "%02d", selectedMinute))")
+//                    .font(.headline)
+//                    .padding(.bottom,30)
+                HStack {
+                    VStack {
+                        Button(action: {
+                            print()
+                        }) {
+                            Image(systemName: "multiply")
+                                .font(.headline)
+                                .foregroundColor(.white)
+                        }
+                        .padding(.trailing, 40)
+                    }
+                    VStack {
+                        Button(action: {
+                            print()
+                        }) {
+                            Image(systemName: "checkmark")
+                                .font(.headline)
+                                .foregroundColor(.pink)
+                        }
+                    }
+                    .padding(.leading, 40)
+                }
+                .padding(.top, 10)
+            }
+        }
+//            .navigationTitle("Time")
+        }
+        
+    }
+
+
+struct CustomPicker: View {
+    @Binding var selectedValue: Int
+    let range: [Int]
+    let label: String
+    
+    var body: some View {
+        VStack {
+//            Text(label)
+//                .font(.headline)
+            Picker("\(label)", selection: $selectedValue) {
+                ForEach(range, id: \.self) { value in
+                    Text("\(value)")
+                        .font(.system(size: 24))
+                        .padding()
+                        .background(value == selectedValue ? Color.clear : Color.clear)
+                        .cornerRadius(8)
+                        .foregroundColor(value == selectedValue ? .pink : .primary)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 8)
+                                .stroke(value == selectedValue ? Color.clear : Color.clear, lineWidth: 2)
+                        )
+                        .tag(value)
+                }
+            }
+            .pickerStyle(.wheel)
+            //.frame(width: 80, height: 140) // Picker의 너비와 높이 조정
+            .clipped()
+        }
     }
 }
 
-#Preview {
-    TimeInputView()
+struct TimeInputView_Previews: PreviewProvider {
+    static var previews: some View {
+        TimeInputView()
+    }
 }
