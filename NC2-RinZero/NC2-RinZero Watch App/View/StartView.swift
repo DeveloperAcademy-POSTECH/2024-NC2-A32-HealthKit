0e9533225407ec1f00b30d0f56f180517309adb5
Taerin Kim <[taerin1109@gmail.com]>
2024-06-19 23:12:28 +0900

    feat: StartView 구현

--- NC2-RinZero/NC2-RinZero Watch App/View/StartView.swift
+++ NC2-RinZero/NC2-RinZero Watch App/View/StartView.swift
@@ -9,7 +9,14 @@ import SwiftUI
 
 struct StartView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image("Frame 18")
+                .resizable()
+                .frame(width: 250, height: 250)
+            Text("START")
+                .foregroundStyle(.white)
+                .font(.system(size: 36, weight: .bold))
+        }
     }
 }
 
